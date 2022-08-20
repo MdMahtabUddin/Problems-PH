93a45fb6b580d683893bd1152d90efe7711e75f4
MdMahtabUddin <mahtab.sentu@outlook.com>
2022-08-21 03:26:57 +0600

    swap and destructing

---
+++ swap.js
@@ -0,0 +1,11 @@
+var first =5;
+var second =7;
+// console.log(first, second);
+
+// var temp = first;
+// var first = second;
+// var second =temp;
+
+// * destructing
+[first, second] =[second,first]
+console.log(first, second);
