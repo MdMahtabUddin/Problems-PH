1b07d5a3aa205ac2af5af671d1ed14fb779bead8
MdMahtabUddin <mahtab.sentu@outlook.com>
2022-08-21 03:04:44 +0600

    added

--- math.js
+++ math.js
@@ -7,7 +7,10 @@
 // const output = Math.ceil(myNumber);
 // Math.celi always return nearest top number 
 
-const myNumber =5.999;
-const output =Math.floor(myNumber);
+// const myNumber =5.999;
+// const output =Math.floor(myNumber);
 // Math.floor always return nearest bottom number 
-console.log(output);
+
+const myNumber =Math.random() * 6;
+const rounded = Math.round(myNumber);
+console.log(rounded);
