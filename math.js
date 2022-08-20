116514b80f4f60cf9b6ec11ef784ddd30d974878
MdMahtabUddin <mahtab.sentu@outlook.com>
2022-08-21 02:40:42 +0600

    added

--- math.js
+++ math.js
@@ -1,6 +1,13 @@
-const myNumber = -5;
-const output = Math.abs(myNumber);
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
 
 // Math.abs always return positive value 
 
+// const myNumber =7.103;
+// const output = Math.ceil(myNumber);
+// Math.celi always return nearest top number 
+
+const myNumber =5.999;
+const output =Math.floor(myNumber);
+// Math.floor always return nearest bottom number 
 console.log(output);
