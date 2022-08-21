fae36375340de686a5a0b98d0f6d75dbedd9ae10
MdMahtabUddin <mahtab.sentu@outlook.com>
2022-08-22 02:55:49 +0600

    fibonacci

--- sum.js
+++ sum.js
@@ -1,8 +1,21 @@
-const number = [45, 66, 70, 27];
-let sum =0;
+// const number = [45, 66, 70, 27];
+// let sum =0;
+// for (let i =0; i< number.length; i++){
+//     const element = number[i];
+    
+//     sum += element;
+//     console.log(sum);
+// }
+
+
+function myNumber (number){
+    let sum =0;
 for (let i =0; i< number.length; i++){
     const element = number[i];
-    
-    sum += element;
-    console.log(sum);
+    sum +=element;
 }
+    return sum;
+}
+const total= myNumber([67,50,45,92]);
+
+console.log(total);
