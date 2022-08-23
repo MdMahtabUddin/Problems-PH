d71abfa9274e94691ce3fdefca0a0f60798f0e35
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-08-24 02:09:55 +0600

    added

---
+++ add-multiply.js
@@ -0,0 +1,20 @@
+
+
+function furnitureCalculator(chairQuantity , bedQuantity , tableQuantity){
+
+const perChairWood =3;
+const perBedWood =30;
+const perTableWood =10;
+
+// wood calculation
+
+const chairWood = chairQuantity * perChairWood;
+const bedWood = bedQuantity * perBedWood;
+const tableWood = tableQuantity *perTableWood;
+
+totalWood = chairWood + bedWood +tableWood;
+return totalWood;
+}
+
+const furnitureMade =furnitureCalculator(10,5,5);
+console.log(furnitureMade);
