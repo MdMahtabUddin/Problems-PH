2b4a9b3705f88abaa0108920d0284a65ebe3c69e
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-08-27 21:17:09 +0000

    added

--- max.js
+++ max.js
@@ -1,1 +1,18 @@
-// PH-21.3
+// PH- 21.3 Find max of two values, find max of three values
+
+const business =700;
+const minister =1500;
+const army =900;
+// compare with 3 
+// if (business > minister && business > army){
+//     console.log('Business is bigger');
+// } else if (minister > business && minister > army){
+//     console.log('minister is bigger');
+// }else{
+//     console.log('army is bigger');
+// }
+
+// another way 
+
+var max = Math.max(business,minister,army);
+console.log('largest is', max);
